06937d23d8766c2952227667381702ed65011258
theocareso <theocareso@gmail.com>
2018-11-28 14:39:19 -0200

    Tela Login Primefaces Netbeans

---
+++ src/java/usuarioBean/Usuario.java
@@ -0,0 +1,86 @@
+package usuarioBean;
+
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@ManagedBean
+@ViewScoped
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable {
+
+    private int id;
+    private String usuario;
+    private String senha;
+
+    @Id
+    @GeneratedValue
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    @Column(name="usuario") 
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    @Column(name="senha") 
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
+    public void validarLogin(String usuario, String senha) {
+        if (getUsuario().equals("ailton") && getSenha().equals("123")) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Bem vindo " + getUsuario()));
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Dados incorretos. "));
+        }
+
+    }
+}
